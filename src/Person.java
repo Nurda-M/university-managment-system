8c754055f71d66032981a8a4c890c582992a1408
Nurda-M <250848@astanait.edu.kz>
2026-01-19 07:37:42 +0500

    Update Person.java

--- src/Person.java
+++ src/Person.java
@@ -1,5 +1,3 @@
-package src;
-
 import java.util.Objects;
 
 public abstract class Person implements Identifiable {
@@ -11,7 +9,6 @@ public abstract class Person implements Identifiable {
         this.name = Objects.requireNonNull(name, "name must not be null");
     }
 
-    @Override
     public String getId() {
         return id;
     }
@@ -19,24 +16,20 @@ public abstract class Person implements Identifiable {
     public String getName() { return name; }
     public void setName(String name) { this.name = Objects.requireNonNull(name); }
 
-    // Полиморфное поведение: подклассы дают специфичное summary
     public abstract String getSummary();
 
-    @Override
     public String toString() {
         return getClass().getSimpleName() + "{id='" + id + "', name='" + name + "'}";
     }
 
-    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
 
         Person person = (Person) o;
-        return id.equals(person.id); // равенство по id
+        return id.equals(person.id);
     }
 
-    @Override
     public int hashCode() {
         return Objects.hash(id);
     }
